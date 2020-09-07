0884b0ca4a0f2da81de539bf9d492aaddffa8646
Deniz Yandim <Deniz.yandim00@gmail.com>
2020-09-07 11:33:34 +0200

    Adding printlines

---
+++ src/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    // Psvm
+    public static void main(String[] args) {
+        // sout
+        System.out.println("Hello World");
+        System.out.println("Programmering er sgu pisse sjovt, når vi først kommer igang!");
+    }
+}
